0952cc178f55dd644bedebcdab1c6ade437220d8
Anna Sas <anna.sas.dev@gmail.com>
2024-12-23 18:44:44 +0100

    Feat: Packagable InfiniLore.Server.Contracts

--- src/tools/Tools.InfiniLore/Program.cs
+++ src/tools/Tools.InfiniLore/Program.cs
@@ -23,7 +23,8 @@ public static class Program {
         //      Sometimes CLI params is not the answer.
         //      Code is the true saviour
         string projects = string.Join(";", 
-            "InfiniLore.Server.Types"
+            "InfiniLore.Server.Types",
+            "InfiniLore.Server.Contracts"
         );
         string oneLineArgs = InputHelper.ToOneLine(args).Replace("%PROJECTS%", projects);
         
