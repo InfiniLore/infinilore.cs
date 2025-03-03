8c32af84bced7d0ddd9daa80d2235eb5c4d33501
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-03 10:57:58 +0100

    Feat: Early idea for stores

---
+++ src/InfiniLore.Server.Services.Auth0/RolesStore.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.Credentials;
+
+namespace InfiniLore.Server.Services.Auth0;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[RolesStore]
+public partial class RolesStore {
+    public static partial string User { get; }
+    public static string[] UserPermissions { get; } = [];
+    
+    public static partial string Consumer { get; }
+    public static string[] ConsumerPermissions { get; } = [
+        ..UserPermissions
+    ];
+    
+    public static partial string Producer { get; }
+    public static string[] ProducerPermissions { get; } = [
+        ..ConsumerPermissions
+    ];
+    
+    public static partial string InfiniloreAdmin { get; }
+    public static string[] InfiniloreAdminPermissions { get; } = [
+        ..ProducerPermissions
+    ];
+    
+    public static partial string InfiniloreDeveloper { get; }
+    public static string[] InfiniloreDeveloperPermissions { get; } = [
+        ..InfiniloreAdminPermissions
+    ];
+}
