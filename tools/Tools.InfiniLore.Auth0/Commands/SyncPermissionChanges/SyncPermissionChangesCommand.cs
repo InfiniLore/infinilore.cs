4cf28916b763fc4317605f1419d981261b9c701e
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-06 12:21:30 +0100

    Feat: Foundation for Tools.InfiniLore.Auth0

---
+++ tools/Tools.InfiniLore.Auth0/Commands/SyncPermissionChanges/SyncPermissionChangesCommand.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.CliArgsParser;
+using InfiniLore.Credentials.Auth0;
+using InfiniLore.Credentials.Auth0.OptionConfiguration;
+using InfiniLore.Credentials.Auth0.Services;
+using InfiniLore.Server.Services.Auth0;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Tools.InfiniLore.Auth0.Setup;
+
+namespace Tools.InfiniLore.Auth0.Commands.SyncPermissionChanges;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[UsedImplicitly]
+[CliArgsCommand("sync-permission")]
+public partial class SyncPermissionChangesCommand : ICommand<SyncPermissionChangesParameters> {
+    private IServiceProvider Provider { get; } = CommandEnvironmentFactory.Create();
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public async Task ExecuteAsync(SyncPermissionChangesParameters parameters) {
+
+        var auth0PermissionService = Provider.GetRequiredService<IAuth0PermissionService>();
+        Auth0Options options = Provider.GetRequiredService<IOptions<Auth0Options>>().Value;
+        var logger = Provider.GetRequiredService<ILogger<SyncPermissionChangesCommand>>();
+        
+        IEnumerable<string>? permissions = PermissionsStore.GetAllPermissions();
+        if (permissions is null) {
+            logger.Error("Permissions store is null");
+            throw new Exception("Permissions store is null");
+        }
+        
+        IEnumerable<PermissionDto> permissionsDtos = permissions
+            .Select(permission => new PermissionDto(permission, string.Empty));
+        
+        await auth0PermissionService.SyncApiPermissionsAsync(options.Domain, permissionsDtos);
+
+    }
+}
+
+
+public record PermissionDto(
+    string PermissionName, 
+    string Description 
+) : IPermissionDto;
---
+++ tools/Tools.InfiniLore.Auth0/Commands/SyncPermissionChanges/SyncPermissionChangesParameters.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.CliArgsParser;
+
+namespace Tools.InfiniLore.Auth0.Commands.SyncPermissionChanges;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public readonly partial struct SyncPermissionChangesParameters : IParameters {
+    [CliArgsParameter("output", "o")]
+    [CliArgsDescription("Do Console Output")]
+    public bool OutputFolder { get; init; } = true;
+
+}
