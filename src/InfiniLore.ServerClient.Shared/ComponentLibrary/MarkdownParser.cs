8b4fc5ad7e6fc84307b27fc5d0723681ae483b37
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-25 14:12:53 +0100

    Fix: Correctly working parser

--- src/InfiniLore.ServerClient.Shared/ComponentLibrary/MarkdownParser.cs
+++ src/InfiniLore.ServerClient.Shared/ComponentLibrary/MarkdownParser.cs
@@ -35,11 +35,15 @@ public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
     private string Evaluator(Match match) {
         if (match.Groups["bold"].Success) return $"<b>{match.Groups[1].Value}</b>";
         if (match.Groups["italic"].Success || match.Groups["italic_"].Success) return $"<i>{match.Groups[2].Value}</i>";
-        if (match.Groups["strike"].Success) return $"<s>{match.Groups[3].Value}</s>";
-        if (match.Groups["code"].Success) return $"<code>{match.Groups[4].Value}</code>";
-        if (match.Groups["link"].Success) return $"<a href=\"{match.Groups[6].Value}\" target=\"_blank\">{match.Groups[5].Value}</a>";
-        if (match.Groups["heading"].Success) return $"<h{match.Groups[8].Value.Length}>{match.Groups[9].Value}</h{match.Groups[8].Value.Length}>";// Heading levels
-
+        if (match.Groups["strike"].Success) return $"<s>{match.Groups[4].Value}</s>";
+        if (match.Groups["code"].Success) return $"<code>{match.Groups[5].Value}</code>";
+        if (match.Groups["link"].Success) return $"<a href=\"{match.Groups[7].Value}\" target=\"_blank\">{match.Groups[6].Value}</a>";
+        if (match.Groups["heading"].Success) {
+            int headingLevel = match.Groups[8].Value.Length;
+            string headingContent = Parse(match.Groups[9].Value);  // Recursively parse inner content (e.g., bold)
+            return $"<h{headingLevel}>{headingContent}</h{headingLevel}>";
+        }
+        
         return match.Value;
     }
 }
