11652e9b3a73f26af85c63d34783b535b73f9c83
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-25 13:49:40 +0100

    Feat: MarkdownParser Service

---
+++ src/InfiniLore.ServerClient.Shared/ComponentLibrary/MarkdownParser.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Text.RegularExpressions;
+
+namespace InfiniLore.ServerClient.Shared.ComponentLibrary;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableService<MarkdownParser>(ServiceLifetime.Singleton)]
+public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
+    [GeneratedRegex("""
+          (?<bold>\*\*(.+?)\*\*)
+        | (?<italic>\*(.+?)\*)
+        | (?<italic_>_(.+?)_)
+        | (?<strike>~~(.+?)~~)
+        | (?<code>`(.+?)`)
+        | (?<link>\[(.+?)\]\((.+?)\))
+        | (?<heading>^(\#{1,6})\s(.+))
+        """, RegexOptions.IgnorePatternWhitespace | RegexOptions.RightToLeft)]
+    private static partial Regex MarkdownRegex { get; }
+
+    public string Parse(string input) {
+        string output = MarkdownRegex.Replace(input, Evaluator);
+        
+        logger.Information("markdown input: {input} html output: {output}", input, output);
+        
+        return output;
+    }
+    
+    private string Evaluator(Match match) {
+        if (match.Groups["bold"].Success) return $"<b>{match.Groups[1].Value}</b>";
+        if (match.Groups["italic"].Success || match.Groups["italic_"].Success) return $"<i>{match.Groups[2].Value}</i>";
+        if (match.Groups["strike"].Success) return $"<s>{match.Groups[3].Value}</s>";
+        if (match.Groups["code"].Success) return $"<code>{match.Groups[4].Value}</code>";
+        if (match.Groups["link"].Success) return $"<a href=\"{match.Groups[6].Value}\" target=\"_blank\">{match.Groups[5].Value}</a>";
+        if (match.Groups["heading"].Success) return $"<h{match.Groups[8].Value.Length}>{match.Groups[9].Value}</h{match.Groups[8].Value.Length}>";// Heading levels
+
+        return match.Value;
+    }
+}
