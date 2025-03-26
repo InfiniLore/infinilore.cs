f09ea3a9f4780eb250e92fe51efbdfe0efb2b67d
Anna Sas <anna.sas.dev@gmail.com>
2025-03-26 16:55:38 +0100

    Feat: Lists

--- src/InfiniLore.ServerClient.Shared/ComponentLibrary/MarkdownParser.cs
+++ src/InfiniLore.ServerClient.Shared/ComponentLibrary/MarkdownParser.cs
@@ -4,6 +4,7 @@
 using CodeOfChaos.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace InfiniLore.ServerClient.Shared.ComponentLibrary;
@@ -13,22 +14,27 @@ namespace InfiniLore.ServerClient.Shared.ComponentLibrary;
 [InjectableService<MarkdownParser>(ServiceLifetime.Singleton)]
 public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
     [GeneratedRegex("""
-          (?<boldAndItalic>(?<!\*)\*\*\*([^*]+?)\*\*\*(?!\*)|(?<!_)___([^_]+?)___(?!_))
-        | (?<bold>(?<!\*)\*\*([^*]+?)\*\*(?!\*)|(?<!_)__([^_]+?)__(?!_))
-        | (?<italic>(?<!\*)\*([^*]+?)\*(?!\*)|(?<!_)_([^_]+?)_(?!_))
+          (?<boldAndItalic>\*\*\*([^*]+?)\*\*\*|___([^_]+?)___)
+        | (?<bold>\*\*([^*]+?)\*\*|__([^_]+?)__)
+        | (?<italic>\*([^*]+?)\*|_([^_]+?)_)
         | (?<strike>~~(.+?)~~)
-        | (?<code>(?<!``)`([^`\n]+?)`(?!``))
+        | (?<code>`((?:[^`\\]|\\`)+?)`)
         | (?<link>\[(.+?)\]\((.+?)\))
-        """, RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline)]
-    private static partial Regex MarkupRegex { get; }
+        """, RegexOptions.IgnorePatternWhitespace)]
+    private static partial Regex SinglelineStructuresRegex { get; }
 
     [GeneratedRegex("""
-        (?<heading>^(\#{1,6})\s(.+))
+          (?<heading>^(\#{1,6})\s(.+))
         | (?<codeBlock>```(.+?)\n([\s\S]*?)```)
-        | (?<headingSimple>^(.+?)\n[-]{3,})
+        | (?<headingSimple>^(.+?)\n[-=]{3,})
+        | (?<listUnordered>(?:^[^\S\r\n]*[*+-]\s+.+(?:(?:\n[^\S\r\n]*[*+-.]\d*\s+.+)|(?:\n[^\S\r\n]+.+))*(?:[^\S\r\n]{0,2}(?![\r\n]))?)+)
+        | (?<listOrdered>(?:^[^\S\r\n]*[*+-.]\d+\s+.+(?:(?:\n[^\S\r\n]*[*+-.]\d+\s+.+)|(?:\n[^\S\r\n]+.+))*(?:[^\S\r\n]{0,2}(?![\r\n]))?)+)
         | (?<remainder>.+?(?:\n|$))
         """, RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline)]
-    private static partial Regex ParagraphRegex { get; }
+    private static partial Regex MultilineStructuresRegex { get; }
+    
+    [GeneratedRegex(@"^[ ]*[*+-.]\d*\s+(.+)((?:(?:(?:\n[ ]+[*+-.]\d*)|(?:\n[ ]+))\s+.+)*)", RegexOptions.Multiline)]
+    private static partial Regex ListItemBodyRegex { get; }
 
     // private static void something() {
     //     builder.AddMarkdownEditor(config =>
@@ -52,51 +58,114 @@ public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
     // }
 
     public string Parse(string input) {
-        string output = ParagraphRegex.Replace(input, evaluator: ParagraphEvaluator);
+        string output = MultilineStructuresRegex.Replace(input, evaluator: MultilineStructuresEvaluator);
         
         logger.Information("markdown input: {input} html output: {output}", input, output);
         return output;
     }
 
-    private static string MarkupRegexEvaluator(Match match, Origin origin = Origin.Undefined) {
+    private static string MultilineStructuresEvaluator(Match match) {
+        if (match.Groups["remainder"] is { Success: true, Value: var paragraph }) {
+            string output = SinglelineStructuresRegex.Replace(paragraph, static m => SinglelineStructuresEvaluator(m));
+            return $"<p>{output}</p>";
+        }
+        
+        if (match.Groups["heading"].Success && match.Groups[1] is { Success: true, Length: var headingLevel } && match.Groups[2] is { Success: true, Value: var string12 }) {
+            string output = SinglelineStructuresRegex.Replace(string12, static m => SinglelineStructuresEvaluator(m));
+            return $"<h{headingLevel}>{output}</h{headingLevel}>";
+        }
+
+        if (match.Groups["codeBlock"].Success && match.Groups[3] is { Success: true, Value: var string13 } && match.Groups[4] is { Success: true, ValueSpan: var chars14 }) {
+            return $"<code><pre>{chars14}</pre></code>"; // todo escape < and > characters
+        }
+
+        if (match.Groups["headingSimple"].Success && match.Groups[5] is { Success: true, Value: var string15 }) {
+            string output = SinglelineStructuresRegex.Replace(string15, static m => SinglelineStructuresEvaluator(m));
+            return $"<h1>{output}</h1>";
+        }
+
+        if (match.Groups["listUnordered"] is { Success: true, Value: var string11 }) {
+            var builder = new StringBuilder(); // todo get and move to pool
+            builder.Append("<ul>");
+            foreach (Match lineMatch in ListItemBodyRegex.Matches(string11)) {
+                builder.Append("<li>");
+                string lineHeader = SinglelineStructuresRegex.Replace(lineMatch.Groups[1].Value, static m => SinglelineStructuresEvaluator(m));
+                builder.Append(lineHeader);
+
+                if (lineMatch.Groups[2] is { Success: true, Value: var string16 }) {
+                    string listBody = MultilineStructuresRegex.Replace(string16, static m => MultilineStructuresEvaluator(m));
+                    builder.Append(listBody);
+                }
+                
+                builder.Append("</li>");
+            }
+            
+            builder.Append("</ul>");
+            return builder.ToString();
+        }
+
+        if (match.Groups["listOrdered"] is { Success: true, Value: var string17 }) {
+            var builder = new StringBuilder(); // todo get and move to pool
+            builder.Append("<ol>");
+            foreach (Match lineMatch in ListItemBodyRegex.Matches(string17)) {
+                builder.Append("<li>");
+                string lineHeader = SinglelineStructuresRegex.Replace(lineMatch.Groups[1].Value, static m => SinglelineStructuresEvaluator(m));
+                builder.Append(lineHeader);
+
+                if (lineMatch.Groups[2] is { Success: true, Value: var string16 }) {
+                    string listBody = MultilineStructuresRegex.Replace(string16, static m => MultilineStructuresEvaluator(m));
+                    builder.Append(listBody);
+                }
+                
+                builder.Append("</li>");
+            }
+            
+            builder.Append("</ol>");
+            return builder.ToString();
+        }
+
+        return match.Value;
+    }
+    
+    private static string SinglelineStructuresEvaluator(Match match, Origin origin = Origin.Undefined) {
         if (origin is not Origin.BoldAndItalic && match.Groups["boldAndItalic"].Success) {
             if (match.Groups[1] is { Success: true, Value: var chars1 }) {
-                string output = MarkupRegex.Replace(chars1, evaluator: static m => MarkupRegexEvaluator(m, Origin.BoldAndItalic));
+                string output = SinglelineStructuresRegex.Replace(chars1, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.BoldAndItalic));
                 return $"<b><i>{output}</i></b>";
             }
 
             if (match.Groups[2] is { Success: true, Value: var chars2 }) {
-                string output = MarkupRegex.Replace(chars2, evaluator: static m => MarkupRegexEvaluator(m, Origin.BoldAndItalic));
+                string output = SinglelineStructuresRegex.Replace(chars2, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.BoldAndItalic));
                 return $"<b>{output}</b>";
             }
         }
 
         if (origin is not Origin.Bold && match.Groups["bold"].Success) {
             if (match.Groups[3] is { Success: true, Value: var chars3 }) {
-                string output = MarkupRegex.Replace(chars3, evaluator: static m => MarkupRegexEvaluator(m, Origin.Bold));
+                string output = SinglelineStructuresRegex.Replace(chars3, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.Bold));
                 return $"<b>{output}</b>";
             }
 
             if (match.Groups[4] is { Success: true, Value: var chars4 }) {
-                string output = MarkupRegex.Replace(chars4, evaluator: static m => MarkupRegexEvaluator(m, Origin.Bold));
+                string output = SinglelineStructuresRegex.Replace(chars4, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.Bold));
                 return $"<b>{output}</b>";
             }
         }
 
         if (origin is not Origin.Italic && match.Groups["italic"].Success) {
             if (match.Groups[5] is { Success: true, Value: var chars5 }) {
-                string output = MarkupRegex.Replace(chars5, evaluator: static m => MarkupRegexEvaluator(m, Origin.Italic));
+                string output = SinglelineStructuresRegex.Replace(chars5, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.Italic));
                 return $"<i>{output}</i>";
             }
 
             if (match.Groups[6] is { Success: true, Value: var chars6 }) {
-                string output = MarkupRegex.Replace(chars6, evaluator: static m => MarkupRegexEvaluator(m, Origin.Italic));
+                string output = SinglelineStructuresRegex.Replace(chars6, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.Italic));
                 return $"<i>{output}</i>";
             }
         }
 
         if (origin is not Origin.Strike && match.Groups["strike"].Success && match.Groups[7] is { Success: true, Value: var chars7 }) {
-            string output = MarkupRegex.Replace(chars7, evaluator: static m => MarkupRegexEvaluator(m, Origin.Strike));
+            string output = SinglelineStructuresRegex.Replace(chars7, evaluator: static m => SinglelineStructuresEvaluator(m, Origin.Strike));
             return $"<s>{output}</s>";
 
         }
@@ -110,33 +179,10 @@ public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
         ) {
             return $"<a href=\"{chars9}\" target=\"_blank\">{chars10}</a>";
         }
-
-
-        return match.Value;
-    }
-
-    private static string ParagraphEvaluator(Match match) {
-        if (match.Groups["remainder"] is { Success: true, Value: var paragraph }) {
-            string output = MarkupRegex.Replace(paragraph, static m => MarkupRegexEvaluator(m));
-            return $"<p>{output}</p>";
-        }
         
-        if (match.Groups["heading"].Success && match.Groups[1] is { Success: true, Length: var headingLevel } && match.Groups[2] is { Success: true, Value: var string12 }) {
-            string output = MarkupRegex.Replace(string12, static m => MarkupRegexEvaluator(m));
-            return $"<h{headingLevel}>{output}</h{headingLevel}>";
-        }
-
-        if (match.Groups["codeBlock"].Success && match.Groups[3] is { Success: true, Value: var string13 } && match.Groups[4] is { Success: true, ValueSpan: var chars14 }) {
-            return $"<code><pre>{chars14}</pre></code>";// todo escape < and > characters
-        }
-
-        if (match.Groups["headingSimple"].Success && match.Groups[5] is { Success: true, Value: var string15 }) {
-            string output = MarkupRegex.Replace(string15, static m => MarkupRegexEvaluator(m));
-            return $"<h1>{output}</h1>";
-        }
-
         return match.Value;
     }
+    
 
     // # ***boldAndItalic*** **bold** *italic* ~~strike~~ `code` [text](https://example.com)  ___boldAndItalic___ __bold__ _italic_
     // ***boldAndItalic*** **bold** *italic* ~~strike~~ `code` [text](https://example.com)  ___boldAndItalic___ __bold__ _italic_
@@ -148,6 +194,7 @@ public partial class MarkdownParser(ILogger<MarkdownParser> logger) {
     // ##### hea
     // ###### he
 
+    // TODO should be flags
     private enum Origin {
         Undefined = 0,
         BoldAndItalic,
