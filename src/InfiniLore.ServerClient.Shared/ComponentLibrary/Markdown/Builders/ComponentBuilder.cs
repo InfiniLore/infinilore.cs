f63c87aa5eea8fac4a005106a3a578224782343c
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-30 18:50:41 +0200

    Feat: Add ComponentTreeBuilder and ComponentBuilder for rendering
    Introduces early concepts `ComponentTreeBuilder` and `ComponentBuilder` classes to streamline the creation of render fragments for Blazor components. These utilities support adding components, properties, and nested child components, enhancing flexibility and maintainability. Also updates the project file to include `Microsoft.AspNetCore.Components` as a dependency.

---
+++ src/InfiniLore.ServerClient.Shared/ComponentLibrary/Markdown/Builders/ComponentBuilder.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace InfiniLore.ServerClient.Shared.ComponentLibrary.Markdown.Builders;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ComponentBuilder(string componentName) {
+    private string ComponentName { get; set; } = componentName;
+    private readonly Dictionary<string, object> _componentProperties = new();
+    private readonly Queue<ComponentBuilder> ChildComponents = new();
+    private string Content { get; set; } = "";
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public ComponentBuilder AddClass(string className) {
+        if (!_componentProperties.TryAdd("class", className)) {
+            _componentProperties["class"] += $" {className}";
+        }
+
+        return this;
+    }
+
+    public ComponentBuilder AddContent(string content) {
+        Content = content;
+        return this;
+    }
+
+    public ComponentBuilder AddChildComponent(string className) {
+        var componentBuilder = new ComponentBuilder(className);
+        ChildComponents.Enqueue(componentBuilder);
+        return componentBuilder;
+    }
+    
+    public void ToRenderFragment(RenderTreeBuilder builder, ref int sequence){
+        builder.OpenElement(sequence++, ComponentName);
+        builder.AddMultipleAttributes(sequence++, _componentProperties);
+        builder.AddContent(sequence++, Content);
+        
+        while (ChildComponents.TryDequeue(out ComponentBuilder? cb)) {
+            cb.ToRenderFragment(builder, ref sequence);
+        }
+        
+        builder.CloseElement();
+    }
+}
---
+++ src/InfiniLore.ServerClient.Shared/ComponentLibrary/Markdown/Builders/ComponentTreeBuilder.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using Microsoft.AspNetCore.Components;
+
+namespace InfiniLore.ServerClient.Shared.ComponentLibrary.Markdown.Builders;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public class ComponentTreeBuilder {
+    private readonly Queue<ComponentBuilder> _componentQueue = new();
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public ComponentBuilder AddComponent(string componentName) {
+        var componentBuilder = new ComponentBuilder(componentName);
+        _componentQueue.Enqueue(componentBuilder);
+        return componentBuilder;
+    }
+
+    public RenderFragment ToRenderFragment()
+        => builder => {
+            int sequence = 0;
+
+            while (_componentQueue.TryDequeue(out ComponentBuilder? cb)) {
+                cb.ToRenderFragment(builder, ref sequence);
+            }
+        };
+}
