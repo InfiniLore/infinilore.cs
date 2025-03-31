b4ce05cb3c4b44b84be82bc2f7e0bcb85313875d
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-03-31 19:36:21 +0200

    Feat: Refactor JWT decoding and enhance Claims Viewer.
    Replaced single JWT decoding method with separate methods for decoding header, payload, and signature. Updated Claims Viewer to display detailed JWT sections, improving clarity and maintainability. Removed redundant JWT payload logic and updated interfaces accordingly.

--- src/InfiniLore.ServerClient.Contracts/Shared/JwtToken/IJwtTokenEncoder.cs
+++ src/InfiniLore.ServerClient.Contracts/Shared/JwtToken/IJwtTokenEncoder.cs
@@ -8,6 +8,9 @@ namespace InfiniLore.ServerClient.Shared.JwtToken;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public interface IJwtTokenEncoder {
-    bool TryDecodeJwt(string token, [NotNullWhen(true)] out string? payload);
+    bool TryDecodeJwtHeader(string token, [NotNullWhen(true)] out string? header);
+    bool TryDecodeJwtPayload(string token, [NotNullWhen(true)] out string? payload);
+    bool TryGetJwtSignature(string token, [NotNullWhen(true)] out string? signature);
+    
     bool TryGetTokenUtcExpiry(string token, out DateTime expiry);
 }
