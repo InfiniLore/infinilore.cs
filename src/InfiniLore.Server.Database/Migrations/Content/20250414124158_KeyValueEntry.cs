50b13a01d5d2b1e85f48544432e077e8cecfdc8e
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-04-14 14:42:38 +0200

    Feat: Migrate KeyValueStore to KeyValueEntry
    Replaces the KeyValueStore table with KeyValueEntry for improved clarity and consistency. Updated migrations and model snapshots to reflect the new table schema and indices.

---
+++ src/InfiniLore.Server.Database/Migrations/Content/20250414124158_KeyValueEntry.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfiniLore.Server.Database.Migrations.Content
+{
+    /// <inheritdoc />
+    public partial class KeyValueEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeyValueStores");
+
+            migrationBuilder.CreateTable(
+                name: "KeyValueEntries",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", maxLength: 2147483646, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyValueEntries", x => x.Key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeyValueEntries_Key",
+                table: "KeyValueEntries",
+                column: "Key",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KeyValueEntries");
+
+            migrationBuilder.CreateTable(
+                name: "KeyValueStores",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", maxLength: 2147483646, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyValueStores", x => x.Key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeyValueStores_Key",
+                table: "KeyValueStores",
+                column: "Key",
+                unique: true);
+        }
+    }
+}
