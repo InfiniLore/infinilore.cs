7953889b32fa79838d0a778b9b196da707fb1ef2
Anna Sas <anna.sas.dev@gmail.com>
2025-05-05 12:53:52 +0200

    Change: Reset of Migrations

---
+++ src/InfiniLore.Server.Database/Migrations/Content/20250505105305_Initial.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfiniLore.Server.Database.Migrations.Content
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InfiniLoreUserModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Auth0IdGoogle = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Auth0Github = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Auth0MailPassword = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Username = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SoftDeleteDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfiniLoreUserModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KeyValueEntryModel",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", maxLength: 2147483646, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KeyValueEntryModel", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LoreScopeModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SoftDeleteDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoreScopeModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MarkdownFileModel",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Source = table.Column<string>(type: "nvarchar(max)", maxLength: 2147483647, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    SoftDeleteDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MarkdownFileModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfiniLoreUserModel_Auth0Github",
+                table: "InfiniLoreUserModel",
+                column: "Auth0Github",
+                unique: true,
+                filter: "[Auth0Github] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfiniLoreUserModel_Auth0IdGoogle",
+                table: "InfiniLoreUserModel",
+                column: "Auth0IdGoogle",
+                unique: true,
+                filter: "[Auth0IdGoogle] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfiniLoreUserModel_Auth0MailPassword",
+                table: "InfiniLoreUserModel",
+                column: "Auth0MailPassword",
+                unique: true,
+                filter: "[Auth0MailPassword] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfiniLoreUserModel_Id",
+                table: "InfiniLoreUserModel",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfiniLoreUserModel_Username",
+                table: "InfiniLoreUserModel",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KeyValueEntryModel_Key",
+                table: "KeyValueEntryModel",
+                column: "Key",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoreScopeModel_Id",
+                table: "LoreScopeModel",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LoreScopeModel_OwnerId",
+                table: "LoreScopeModel",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OwnerId_Name_Unique",
+                table: "LoreScopeModel",
+                columns: new[] { "OwnerId", "Name" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkdownFileModel_Id",
+                table: "MarkdownFileModel",
+                column: "Id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MarkdownFileModel_OwnerId",
+                table: "MarkdownFileModel",
+                column: "OwnerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InfiniLoreUserModel");
+
+            migrationBuilder.DropTable(
+                name: "KeyValueEntryModel");
+
+            migrationBuilder.DropTable(
+                name: "LoreScopeModel");
+
+            migrationBuilder.DropTable(
+                name: "MarkdownFileModel");
+        }
+    }
+}
--- tests/Fakers.InfiniLore.Server/LoreScopeFaker.cs
+++ tests/Fakers.InfiniLore.Server/LoreScopeFaker.cs
@@ -10,30 +10,30 @@ namespace Fakers.InfiniLore.Server;
 // Code
 // ---------------------------------------------------------------------------------------------------------------------
 public class LoreScopeFaker {
-    private readonly ConcurrentDictionary<Guid, LoreScope> Entries = new();
+    private readonly ConcurrentDictionary<Guid, LoreScopeModel> Entries = new();
 
-    private static Faker<LoreScope> Faker { get; } = new Faker<LoreScope>()
+    private static Faker<LoreScopeModel> Faker { get; } = new Faker<LoreScopeModel>()
         .RuleFor(property: x => x.Id, setter: f => f.Random.Guid())
         .RuleFor(property: x => x.OwnerId, setter: f => f.Random.Guid())
-        .RuleFor(property: x => x.Name, setter: f => f.Random.AlphaNumeric(LoreScope.Defaults.NameMaxLength))
+        .RuleFor(property: x => x.Name, setter: f => f.Random.AlphaNumeric(LoreScopeModel.Defaults.NameMaxLength))
         .RuleFor(property: x => x.ShortDescription, setter: f =>
                 f.Random.Bool(0.2f)// 20% chance of empty description
                     ? string.Empty
-                    : f.Lorem.Letter(LoreScope.Defaults.ShortDescriptionMaxLength)// Random string
+                    : f.Lorem.Letter(LoreScopeModel.Defaults.ShortDescriptionMaxLength)// Random string
         );
 
-    private static LoreScope EntryWithFixedId(Guid fixedId, Guid ownerId) => new() {
+    private static LoreScopeModel EntryWithFixedId(Guid fixedId, Guid ownerId) => new() {
         Id = fixedId,
         OwnerId = ownerId,
         Name = Faker.Generate().Name,
         ShortDescription = Faker.Generate().ShortDescription
     };
 
-    public LoreScope GetById(Guid id, Guid ownerId) => Entries.GetOrAdd(
+    public LoreScopeModel GetById(Guid id, Guid ownerId) => Entries.GetOrAdd(
         id,
         valueFactory: static (guid, o) => EntryWithFixedId(guid, o),
         ownerId
     );
 
-    public static LoreScope Generate() => Faker.Generate();
+    public static LoreScopeModel Generate() => Faker.Generate();
 }
