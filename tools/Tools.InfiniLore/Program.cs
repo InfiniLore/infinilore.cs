d921717c4709bb7719d66501692b6c492390549a
Anna Sas <anna.sas.dev@gmail.com>
2025-05-24 15:35:11 +0200

    Feat: Add Serilog and Microsoft.Extensions.Logging integration

--- tools/Tools.InfiniLore/Program.cs
+++ tools/Tools.InfiniLore/Program.cs
@@ -3,6 +3,9 @@
 // ---------------------------------------------------------------------------------------------------------------------
 using CodeOfChaos.CliArgsParser;
 using CodeOfChaos.CliArgsParser.Library;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Tools.InfiniLore;
 // ---------------------------------------------------------------------------------------------------------------------
@@ -10,11 +13,14 @@ namespace Tools.InfiniLore;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class Program {
     public static async Task Main(string[] args) {
+        IServiceProvider provider = InitializeServiceProvider();
+
         // Register & Build the parser
         //      Don't forget to add the current assembly if you built more tools for the current project
         ICliParser parser = CliParser.CreateBuilder()
             .AddFromAssembly<IAssemblyEntry>()
             .AddFromAssembly(typeof(Program).Assembly)
+            .WithServiceProvider(provider)
             .Build();
 
         // We are doing this here because else the launchSettings.json file becomes a humongous issue to deal with.
@@ -27,5 +33,29 @@ public static class Program {
 
         string oneLineArgs = ArgsInputHelper.ToOneLine(args).Replace("%PROJECTS%", projects);
         await parser.ExecuteAsync(oneLineArgs);
+    }
+    
+    private static IServiceProvider InitializeServiceProvider() {
+        var services = new ServiceCollection();
+
+        #region Logging
+        LoggerConfiguration loggerConfig = new LoggerConfiguration()
+            .AsAnnaSasDevServerConsole(
+                24,
+                configure: asyncConsoleConfig => asyncConsoleConfig.ApplyThemeToRedirectedOutput = true// Needed for nice DotnetWatch console output    
+            )
+            .WithTruncateSourceContextEnricher(maxLength: 24);
+
+        Log.Logger = loggerConfig.CreateLogger();
+
+        // Clear default providers and setup Serilog
+        services.AddLogging(loggingBuilder => {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.AddSerilog(Log.Logger, dispose: true);
+        });
+        services.AddSingleton(Log.Logger);
+        #endregion
+        
+        return services.BuildServiceProvider();
     }
 }
