efee474facce8c202da1fdc2f34ae7646fc41302
Anna Sas <anna.sas.dev@gmail.com>
2025-05-26 15:36:44 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const plugin = require('tailwindcss/plugin')
 
 module.exports = {
     content: [
-        "./**/*.{razor,html,cshtml}"
+        "./**/*.{razor,html,cshtml,cs}"
     ],
     theme: {
         extend: {
