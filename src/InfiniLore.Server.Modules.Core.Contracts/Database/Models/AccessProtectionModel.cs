6cace5fd9da68d29882efd8551e07ae0463c9457
Anna Sas <anna.sas.dev@gmail.com>
2025-05-27 17:34:13 +0200

    Feat: Add tests and enhance permission handling in models

--- src/InfiniLore.Server.Modules.Core.Contracts/Database/Models/AccessProtectionModel.cs
+++ src/InfiniLore.Server.Modules.Core.Contracts/Database/Models/AccessProtectionModel.cs
@@ -11,9 +11,9 @@ namespace InfiniLore.Server.Modules.Core.Database;
 public class AccessProtectionModel : BasicModel {
     public Guid ProtectedModelId { get; init; } = Guid.Empty;
     public Guid ModelOwnerId { get; init; } = Guid.Empty;
-    public InfiniLoreUserModel? ModelOwner { get; set; } = null;
-    
-    public ICollection<AccessProtectionRuleModel>? Rules { get; set; } = null;
+    public InfiniLoreUserModel? ModelOwner { get; set; }
+
+    public ICollection<AccessProtectionRuleModel>? Rules { get; set; }
 
     [NotMapped] public FrozenDictionary<Guid, AccessProtectionRuleModel[]> UserMappedRules => Rules?
             .GroupBy(rule => rule.UserId)
@@ -23,37 +23,44 @@ public class AccessProtectionModel : BasicModel {
     // -----------------------------------------------------------------------------------------------------------------
     // Methods
     // -----------------------------------------------------------------------------------------------------------------
-    public bool HasPermission(Guid userId, string permission) {
+    public bool HasPermission(Guid userId, string? permission = null) {
         if (userId == ModelOwnerId) return true;
+        if (permission.IsNullOrWhiteSpace()) return false;
+
         return UserMappedRules.TryGetValue(userId, out AccessProtectionRuleModel[]? rules)
             && rules.Any(rule => rule.Permission == permission);
     }
 
-    public void GrantPermission(Guid userId, string permission) {
-        if (userId == ModelOwnerId) return;
+    public bool TryGrantPermission(Guid userId, string permission) {
+        if (userId == ModelOwnerId) return true;
+        if (permission.IsNullOrWhiteSpace()) return false;
+        if (permission.Length > AccessProtectionRuleModel.Defaults.PermissionMaxLength) return false;
+
         if (!UserMappedRules.TryGetValue(userId, out AccessProtectionRuleModel[]? rules)) {
             Rules ??= new List<AccessProtectionRuleModel>();
             Rules.Add(new AccessProtectionRuleModel {
                 UserId = userId,
                 Permission = permission
             });
-            return;
+            return true;
         }
-        
+
         // If the permission is already granted, return.
-        if (rules.FirstOrDefault(r => r.Permission == permission) is not null) return;
-        
+        if (rules.FirstOrDefault(r => r.Permission == permission) is not null) return false;
+
         Rules ??= new List<AccessProtectionRuleModel>();
         Rules.Add(new AccessProtectionRuleModel {
             UserId = userId,
             Permission = permission
         });
+        return true;
     }
 
-    public void RevokePermission(Guid userId, string permission) {
-        if (userId == ModelOwnerId) return;
-        if (!UserMappedRules.TryGetValue(userId, out AccessProtectionRuleModel[]? rules)) return;
-        if (rules.FirstOrDefault(r => r.Permission == permission) is not {} foundRule) return;
-        Rules?.Remove(foundRule);
+    public bool TryRevokePermission(Guid userId, string permission) {
+        if (userId == ModelOwnerId) return false;
+        if (!UserMappedRules.TryGetValue(userId, out AccessProtectionRuleModel[]? rules)) return false;
+        if (rules.FirstOrDefault(r => r.Permission == permission) is not {} foundRule) return false ;
+
+        return Rules?.Remove(foundRule) ?? false;
     }
 }
