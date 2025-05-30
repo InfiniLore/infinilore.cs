acbaab16817ba035e07233d39eb6f8457c27edf4
Anna Sas <anna.sas.dev@gmail.com>
2025-05-30 18:40:12 +0200

    Feat: Reintroduce LsMarkdownFiles module with .NET 9 projects

---
+++ src/InfiniLore.Modules.LsMarkdownFiles.Server/LsMarkdownFilesModuleSetup.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.Modules.Core.Server;
+using InfiniLore.Modules.LsMarkdownFiles.Shared;
+using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InfiniLore.Modules.LsMarkdownFiles.Server;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[UsedImplicitly]
+public class LsMarkdownFilesModuleSetup : ServerModuleSetup {
+    public override void SetupServices(IServiceCollection services) {
+        services.RegisterServicesFromInfiniLoreModulesLsMarkdownFilesShared();
+        services.RegisterServicesFromInfiniLoreModulesLsMarkdownFilesServer();
+    }
+}
