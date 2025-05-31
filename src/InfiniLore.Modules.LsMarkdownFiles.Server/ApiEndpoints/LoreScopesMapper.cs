28f96bdb74bccca1f4ebc4e380d87e2bd80d34f0
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-05-31 22:38:57 +0200

    Feat: Introduce API endpoints for managing LsMarkdownFiles and their responses

---
+++ src/InfiniLore.Modules.LsMarkdownFiles.Server/ApiEndpoints/LoreScopesMapper.cs
@@ -0,0 +1,13 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Extensions.DependencyInjection;
+using InfiniLore.Modules.Core.Server.ApiEndpoints;
+using InfiniLore.Server.Modules.LsMarkdownFiles.Database;
+
+namespace InfiniLore.Modules.LsMarkdownFiles.Server.ApiEndpoints;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[InjectableSingleton<LoreScopesMapper>]
+public class LoreScopesMapper : AutoResponsesMapper<LsMarkdownFileResponse, LsMarkdownFileMapper, LsMarkdownFilesResponse, LsMarkdownFileModel>;
---
+++ src/InfiniLore.Modules.LsMarkdownFiles.Server/ApiEndpoints/LsMarkdownFileResponse.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+
+using InfiniLore.Modules.Core.Server.ApiEndpoints;
+using JetBrains.Annotations;
+
+namespace InfiniLore.Modules.LsMarkdownFiles.Server.ApiEndpoints;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public record LsMarkdownFileResponse : OwnedResponse {
+    public required string Name { [UsedImplicitly] get; init; }
+    public required Guid LastUserToEditId { [UsedImplicitly] get; init; }
+    public required string? ResourceUrl { [UsedImplicitly] get; init; }
+}
