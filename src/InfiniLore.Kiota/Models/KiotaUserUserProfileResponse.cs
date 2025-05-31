7a9bf6cfa3fed0cfe6cd96d334b4985cc28042ba
Anna Sas <anna.sas.dev@gmail.com>
2025-05-31 16:57:22 +0200

    Feat: Add paginated user profile response and query support

--- src/InfiniLore.Kiota/Models/KiotaUserUserProfileResponse.cs
+++ src/InfiniLore.Kiota/Models/KiotaUserUserProfileResponse.cs
@@ -9,11 +9,19 @@ namespace InfiniLore.Kiota.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class KiotaUserUserProfileResponse : global::InfiniLore.Kiota.Models.KiotaBasicResponse, IAdditionalDataHolder, IParsable
+    public partial class KiotaUserUserProfileResponse : IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>The createdDate property</summary>
+        public DateTimeOffset? CreatedDate { get; set; }
+        /// <summary>The id property</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? Id { get; set; }
+#nullable restore
+#else
+        public string Id { get; set; }
+#endif
         /// <summary>The username property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
@@ -23,18 +31,11 @@ namespace InfiniLore.Kiota.Models
         public string Username { get; set; }
 #endif
         /// <summary>
-        /// Instantiates a new <see cref="global::InfiniLore.Kiota.Models.KiotaUserUserProfileResponse"/> and sets the default values.
-        /// </summary>
-        public KiotaUserUserProfileResponse() : base()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
-        /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
         /// <returns>A <see cref="global::InfiniLore.Kiota.Models.KiotaUserUserProfileResponse"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::InfiniLore.Kiota.Models.KiotaUserUserProfileResponse CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::InfiniLore.Kiota.Models.KiotaUserUserProfileResponse CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
             return new global::InfiniLore.Kiota.Models.KiotaUserUserProfileResponse();
@@ -43,10 +44,12 @@ namespace InfiniLore.Kiota.Models
         /// The deserialization information for the current model
         /// </summary>
         /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public override IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
         {
-            return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
+            return new Dictionary<string, Action<IParseNode>>
             {
+                { "createdDate", n => { CreatedDate = n.GetDateTimeOffsetValue(); } },
+                { "id", n => { Id = n.GetStringValue(); } },
                 { "username", n => { Username = n.GetStringValue(); } },
             };
         }
@@ -54,12 +57,12 @@ namespace InfiniLore.Kiota.Models
         /// Serializes information the current object
         /// </summary>
         /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public override void Serialize(ISerializationWriter writer)
+        public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            base.Serialize(writer);
+            writer.WriteDateTimeOffsetValue("createdDate", CreatedDate);
+            writer.WriteStringValue("id", Id);
             writer.WriteStringValue("username", Username);
-            writer.WriteAdditionalData(AdditionalData);
         }
     }
 }
