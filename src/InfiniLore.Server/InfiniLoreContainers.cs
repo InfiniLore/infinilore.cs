6838f802a94bededa1c162648bae50fb12e0a47d
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-06-01 23:09:28 +0200

    Change: Remove Minio Web UI port binding in `InfiniLoreContainers`
    Deleted the `MinioWebUiPort` constant and its corresponding port binding in the Minio container setup to streamline configuration.

--- src/InfiniLore.Server/InfiniLoreContainers.cs
+++ src/InfiniLore.Server/InfiniLoreContainers.cs
@@ -22,7 +22,6 @@ public class InfiniLoreContainers : IAsyncDisposable {
     private const string SqlPassword = "AnnaIsTrans4Ever!";
 
     private const int MinioPort = 40627;
-    private const int MinioWebUiPort = 40628;
     
     private const string MinioAccessKey = "minioadmin";
     private const string MinioSecretKey = "minioadmin";
@@ -58,7 +57,6 @@ public class InfiniLoreContainers : IAsyncDisposable {
             .WithLogger(Logger)
             .WithImage(MinioImage)
             .WithPortBinding(MinioPort, MinioBuilder.MinioPort)
-            .WithPortBinding(MinioWebUiPort, 40585)
             .WithUsername(MinioAccessKey)
             .WithPassword(MinioSecretKey)
             .WithName("infinilore-dev-s3files")
