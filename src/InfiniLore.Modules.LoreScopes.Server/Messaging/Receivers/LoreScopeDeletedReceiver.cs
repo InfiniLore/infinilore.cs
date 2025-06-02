c79126bd3929892e38b28ed0a5ad6f0ee98ebb17
Anna Sas <anna.sas.dev@gmail.com>
2025-06-02 13:24:53 +0200

    Feat: Add LoreScopeRemoved and LoreScopeDeleted receivers

---
+++ src/InfiniLore.Modules.LoreScopes.Server/Messaging/Receivers/LoreScopeDeletedReceiver.cs
@@ -0,0 +1,55 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using AterraEngine.Unions;
+using CodeOfChaos.Types.UnitOfWork;
+using FastEndpoints;
+using InfiniLore.Modules.Core.Server.Database;
+using InfiniLore.Server.Modules.LoreScopes.Database;
+using InfiniLore.Server.Modules.LoreScopes.Messaging.Notifications;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+
+namespace InfiniLore.Modules.LsMarkdownFiles.Server.Messaging.Receivers;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[UsedImplicitly]
+public class LoreScopeDeletedReceiver(
+    IUnitOfWorkFactory unitOfWorkFactory,
+    ILogger<LoreScopeDeletedReceiver> logger
+) : IEventHandler<LoreScopeDeletedEvent> {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public async Task HandleAsync(LoreScopeDeletedEvent eventModel, CancellationToken ct) {
+        await using IUnitOfWork unitOfWork = unitOfWorkFactory.Create();
+        var s3FileMetaDataRepository = await unitOfWork.GetRepositoryAsync<IS3FileRepository>(ct);
+        var loreScopeRepo = await unitOfWork.GetRepositoryAsync<ILoreScopeRepository>(ct);
+
+        Result<LoreScopeModel> modelResult = await loreScopeRepo.GetByIdAsync(
+            eventModel.LoreScopeId,
+            QueryConfig.WithRetrieveSoftDeleted,
+            ct: ct
+        );
+
+        if (!modelResult.TryGetAsSuccess(out LoreScopeModel? model)) {
+            logger.Warning("Failed to find lorescope with id {LoreScopeId}", eventModel.LoreScopeId);
+            return;
+        }
+
+        if (model.PosterImageMetaDataId is not {} posterImageMetaDataId) {
+            logger.Warning("LoreScope {id} did not have a poster image ", eventModel.LoreScopeId);
+            return;
+        }
+        
+        Result result = await s3FileMetaDataRepository.DeleteByIdAsync(posterImageMetaDataId, ct);
+        if (result.TryGetAsState(out bool? deleted) || deleted is false) {
+            logger.Warning("Failed to delete poster image for lorescope {LoreScopeId}", eventModel.LoreScopeId);
+            return;
+        }
+
+        await unitOfWork.TryCommitTransactionAsync(ct);
+    }
+}
---
+++ src/InfiniLore.Modules.LoreScopes.Server/Messaging/Receivers/LoreScopeRemovedReceiver.cs
@@ -0,0 +1,82 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using AterraEngine.Unions;
+using CodeOfChaos.Types.UnitOfWork;
+using FastEndpoints;
+using InfiniLore.Modules.Core.Server;
+using InfiniLore.Modules.Core.Server.Database;
+using InfiniLore.Server.Modules.LoreScopes.Database;
+using InfiniLore.Server.Modules.LoreScopes.Messaging.Notifications;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+
+namespace InfiniLore.Modules.LoreScopes.Server.Messaging.Receivers;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+[UsedImplicitly]
+public class LoreScopeRemovedReceiver(
+    IUnitOfWorkFactory unitOfWorkFactory,
+    IS3FileStorage fileStorage,
+    ILogger<LoreScopeRemovedReceiver> logger
+) : IEventHandler<LoreScopeRemovedEvent> {
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public async Task HandleAsync(LoreScopeRemovedEvent eventModel, CancellationToken ct) {
+        await using IUnitOfWork unitOfWork = unitOfWorkFactory.Create();
+        var s3FileMetaDataRepository = await unitOfWork.GetRepositoryAsync<IS3FileRepository>(ct);
+        var loreScopeRepo = await unitOfWork.GetRepositoryAsync<ILoreScopeRepository>(ct);
+        string bucketName = S3BucketNames.GetLoreScopeBucket(eventModel.LoreScopeId);
+
+        Result<LoreScopeModel> modelResult = await loreScopeRepo.GetByIdAsync(
+            eventModel.LoreScopeId,
+            new QueryConfig {
+                OptionalInclude = true,
+                RetrieveSoftDeleted = true
+            },
+            ct: ct
+        );
+
+        if (!modelResult.TryGetAsSuccess(out LoreScopeModel? model)) {
+            logger.Warning("Failed to find lorescope with id {LoreScopeId}", eventModel.LoreScopeId);
+            return;
+        }
+
+        if (model.PosterImageMetaData is not {} posterModel) {
+            logger.Warning("LoreScope {id} did not have a poster image ", eventModel.LoreScopeId);
+            return;
+        }
+        
+        string fileName = posterModel.FileName;
+        
+        Result result = await s3FileMetaDataRepository.RemoveAsync(posterModel, ct);
+        if (!result.TryGetAsState(out bool? removed) || removed is false) {
+            logger.Warning("Failed to remove poster image for lorescope {LoreScopeId}", eventModel.LoreScopeId);
+            return;
+        }
+
+        await unitOfWork.TryCommitTransactionAsync(ct);
+        
+        // Remove from S3, only after db has been commited
+        Result s3Result = await fileStorage.TryRemoveFileAsync(
+            bucketName,
+            fileName,
+            ct
+        );
+        
+        if (!s3Result.TryGetAsState(out removed) || removed is false){
+            logger.Warning("Failed to remove poster image for lorescope {LoreScopeId}", eventModel.LoreScopeId);
+        }
+
+        Result bucketRemovedResult = await fileStorage.TryRemoveBucketAsync(bucketName, ct);
+        if (!bucketRemovedResult.TryGetAsState(out removed) || removed is false) {
+            logger.Warning("Failed to remove bucket for lorescope {LoreScopeId}", eventModel.LoreScopeId);
+        }
+        
+        logger.Information("Successfully removed lorescope {LoreScopeId} from S3 and database", eventModel.LoreScopeId);
+
+    }
+}
