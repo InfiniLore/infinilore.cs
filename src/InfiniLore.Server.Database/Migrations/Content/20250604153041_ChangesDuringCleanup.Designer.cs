02b22e007437f6920d4b3be42fa4ba7848fa8329
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-06-04 17:33:12 +0200

    Refactor: Update foreign key delete behavior to `NoAction` and regenerate migration snapshot

--- src/InfiniLore.Server.Database/Migrations/Content/20250604153041_ChangesDuringCleanup.Designer.cs
+++ src/InfiniLore.Server.Database/Migrations/Content/20250604153041_ChangesDuringCleanup.Designer.cs
@@ -4,6 +4,7 @@ using InfiniLore.Server.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InfiniLore.Server.Database.Migrations.Content
 {
     [DbContext(typeof(ContentDb))]
-    partial class ContentDbModelSnapshot : ModelSnapshot
+    [Migration("20250604153041_ChangesDuringCleanup")]
+    partial class ChangesDuringCleanup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
