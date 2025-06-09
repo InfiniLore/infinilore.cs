de86defae74d45483b6bbe8b95e809749da735f0
Anna Sas <74367457+AnnaSasDev@users.noreply.github.com>
2025-06-09 18:38:38 +0200

    Fix: Proper notification in wasm mode on file save

--- src/InfiniLore.Modules.LsMarkdownFiles.Wasm/Services/InteractiveApi/InteractiveApiWasmLsMarkdownFile.cs
+++ src/InfiniLore.Modules.LsMarkdownFiles.Wasm/Services/InteractiveApi/InteractiveApiWasmLsMarkdownFile.cs
@@ -99,14 +99,12 @@ public class InteractiveApiWasmLsMarkdownFile(
                 new MemoryStream(Encoding.UTF8.GetBytes(knownFileId.ToString()))
             );
 
-            Stream? result = await requestBuilder.PostAsync(multipartBody, cancellationToken: ct);
-            if (result is null) return Outcome.FromError(interactiveApi.DefaultApiError);
-
+            await requestBuilder.PostAsync(multipartBody, cancellationToken: ct);
             return Outcome.FromState(true);
         }
         catch (Exception e) {
             logger.Error(e, "Could not upsert ls markdown file");
-            return Outcome.FromError("Could not upsert ls markdown file");
+            return Outcome.FromError($"Could not upsert ls markdown file: {e}");
         }
     }
     
@@ -123,9 +121,7 @@ public class InteractiveApiWasmLsMarkdownFile(
             InfiniLoreApiClient client = interactiveApi.ApiClient;
             WithMarkdownFileItemRequestBuilder requestBuilder = client.Api.V1.DataLorescope[loreScopeId].MarkdownFile[lsMarkdownFileId];
 
-            Stream? result = await requestBuilder.DeleteAsync(cancellationToken: ct);
-
-            if (result is null) return Outcome.FromError(interactiveApi.DefaultApiError);
+            await requestBuilder.DeleteAsync(cancellationToken: ct);
             return Outcome.FromState(true);
         }
         catch (Exception e) {
