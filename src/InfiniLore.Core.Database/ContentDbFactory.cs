de850cbda34e99a9fac07333fa5379e788870b4d
Anna Sas <anna.sas.dev@gmail.com>
2025-11-11 15:31:53 +0100

    Feat: Add `InfiniLore.Core.Database` project with `ContentDb` and factory for database management
    Integrated database context for content handling with read-only support and model configuration, added necessary dependencies, and updated solution/project references.

---
+++ src/InfiniLore.Core.Database/ContentDbFactory.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using CodeOfChaos.Types.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace InfiniLore.Core.Database;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class ContentDbFactory {
+    private static IEnumerable<Assembly> _assembliesToImport = [];
+
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
+    public static void RegisterDatabase(
+        IServiceCollection services, 
+        IEnumerable<Assembly> assembliesToImport,
+        Action<DbContextOptionsBuilder> optionsAction
+    ) {
+        _assembliesToImport = assembliesToImport;
+        
+        services.AddDbContextFactory<ContentDb>(optionsAction.Invoke);
+        services.AddReadonlyUnitOfWork<ContentDb>();
+    }
+    
+    internal static void ConfigureModel(ModelBuilder modelBuilder) {
+        foreach (Assembly assembly in _assembliesToImport) {
+            modelBuilder.ApplyConfigurationsFromAssembly(assembly);
+        }
+    }
+}
