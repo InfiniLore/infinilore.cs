56f188ae06cbb1d5d3539e59aba14b190725f168
Anna Sas <anna.sas.dev@gmail.com>
2025-11-19 15:04:19 +0100

    Upgrade dependencies and enable static web assets configuration
    Upgraded Microsoft.CodeAnalysis to 5.0.0 and TUnit to 1.2.11 in Directory.Packages.props. Added missing Microsoft.CodeAnalysis package reference to Tests.Core.Modular. Enabled use of static web assets in Program.cs with `UseStaticWebAssets` and updated app configurations accordingly.

--- src/application/InfiniLore.Application.Desktop/Program.cs
+++ src/application/InfiniLore.Application.Desktop/Program.cs
@@ -11,6 +11,7 @@ using InfiniLore.Modules.Projects;
 using InfiniLore.Modules.Users;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -53,6 +54,8 @@ public static class Program {
         
         appBuilder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
+
+        appBuilder.WebHost.UseStaticWebAssets();
         
         // -------------------------------------------------------------------------------------------------------------
         // Application
@@ -66,41 +69,41 @@ public static class Program {
 
         app.UseAntiforgery();
 
-        // app.MapStaticAssets();
+        app.MapStaticAssets();
         app.MapRazorComponents<App>()
             .AddInteractiveServerRenderMode();
         
         infiniFrameServer.MapInfiniFrameJsEndpoints();
         
-        infiniFrameServer.WebApp.MapGet("/_content/InfiniLore.InfiniBlazor/InfiniBlazor.js", requestDelegate: async context => {
-            Assembly assembly = typeof(InfiniBlazorConfig).Assembly;
-            const string resourceName = "InfiniLore.InfiniBlazor.wwwroot.InfiniBlazor.js";
-
-            await using Stream? stream = assembly.GetManifestResourceStream(resourceName);
-            if (stream == null) {
-                context.Response.StatusCode = 404;
-                await context.Response.WriteAsync("Resource not found");
-                return;
-            }
-
-            context.Response.ContentType = "application/javascript";
-            await stream.CopyToAsync(context.Response.Body);
-        });
-        
-        infiniFrameServer.WebApp.MapGet("/_content/InfiniLore.InfiniBlazor/InfiniBlazor.css", requestDelegate: async context => {
-            Assembly assembly = typeof(InfiniBlazorConfig).Assembly;
-            const string resourceName = "InfiniLore.InfiniBlazor.wwwroot.InfiniBlazor.css";
-
-            await using Stream? stream = assembly.GetManifestResourceStream(resourceName);
-            if (stream == null) {
-                context.Response.StatusCode = 404;
-                await context.Response.WriteAsync("Resource not found");
-                return;
-            }
-
-            context.Response.ContentType = "text/css";
-            await stream.CopyToAsync(context.Response.Body);
-        });
+        // infiniFrameServer.WebApp.MapGet("/_content/InfiniLore.InfiniBlazor/InfiniBlazor.js", requestDelegate: async context => {
+        //     Assembly assembly = typeof(InfiniBlazorConfig).Assembly;
+        //     const string resourceName = "InfiniLore.InfiniBlazor.wwwroot.InfiniBlazor.js";
+        //
+        //     await using Stream? stream = assembly.GetManifestResourceStream(resourceName);
+        //     if (stream == null) {
+        //         context.Response.StatusCode = 404;
+        //         await context.Response.WriteAsync("Resource not found");
+        //         return;
+        //     }
+        //
+        //     context.Response.ContentType = "application/javascript";
+        //     await stream.CopyToAsync(context.Response.Body);
+        // });
+        //
+        // infiniFrameServer.WebApp.MapGet("/_content/InfiniLore.InfiniBlazor/InfiniBlazor.css", requestDelegate: async context => {
+        //     Assembly assembly = typeof(InfiniBlazorConfig).Assembly;
+        //     const string resourceName = "InfiniLore.InfiniBlazor.wwwroot.InfiniBlazor.css";
+        //
+        //     await using Stream? stream = assembly.GetManifestResourceStream(resourceName);
+        //     if (stream == null) {
+        //         context.Response.StatusCode = 404;
+        //         await context.Response.WriteAsync("Resource not found");
+        //         return;
+        //     }
+        //
+        //     context.Response.ContentType = "text/css";
+        //     await stream.CopyToAsync(context.Response.Body);
+        // });
         
         infiniFrameServer.Run();
 
