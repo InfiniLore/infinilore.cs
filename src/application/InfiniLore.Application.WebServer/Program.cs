cd2b0f91c0027fe6d7078eaea5da4acc715997d3
Anna Sas <anna.sas.dev@gmail.com>
2025-11-25 18:34:26 +0100

    Feat: Add core reconnect modal and database initialization for application
    - Introduced a reconnect modal using Blazor components, Razor, and JavaScript for handling connection state changes.
    - Added SQLite database integration with `InfiniLoreDb` and ensured its initialization in the application startup.
    - Updated `InfiniLore.Application` to centralize application logic and module dependencies.
    - Integrated `FastEndpoints` and Swagger for enhanced API definition and connection retries.
    - Updated solution and project files to include new application and modal assets.
    - Modified layout components (`NavBarLeft`, `App.razor`) to include development-only features like API Docs links and reconnect modals.

--- src/application/InfiniLore.Application.WebServer/Program.cs
+++ src/application/InfiniLore.Application.WebServer/Program.cs
@@ -1,8 +1,18 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
+using FastEndpoints;
+using FastEndpoints.Swagger;
 using InfiniLore.Core;
+using InfiniLore.Core.Database;
+using InfiniLore.Core.Modular;
+using InfiniLore.Modules.Assets;
+using InfiniLore.Modules.Projects;
+using InfiniLore.Modules.Users;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace InfiniLore.Application.WebServer;
 // ---------------------------------------------------------------------------------------------------------------------
@@ -15,15 +25,45 @@ public static class Program {
         // -------------------------------------------------------------------------------------------------------------
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+        builder.Services.AddInfiniModuleProvider(moduleCollection => {
+                                                        moduleCollection.AddModule<UsersInfiniModule>();
+                                                        moduleCollection.AddModule<ProjectsInfiniModule>();
+                                                        moduleCollection.AddModule<AssetsInfiniModule>();
+                                                    },
+                                                    out InfiniModuleProvider moduleProvider
+        );
+        
+        builder.Services.AddInfiniLoreDb(
+            options => {
+                const string dbFile = "InfiniLore.db";
+                var connection = new SqliteConnection($"DataSource={dbFile}");
+                connection.Open();
+
+                options.UseSqlite(connection);
+            },
+            moduleProvider.Assemblies
+        );
+        
+        builder.Services.AddLogging(config => {
+            config.ClearProviders();
+            config.AddSerilog();
+        });
+        
+        builder.Services.AddSerilog(config => {
+            config.AsAnnaSasDevServerConsole(24).MinimumLevel.Debug();
+        });
+        
         builder.Services.AddInfiniBlazor(config => {
             config.Components.SetRenderMode(RenderMode.InteractiveServer);
         });
         
-        // Add services to the container.
         builder.Services.AddRazorComponents()
             .AddInteractiveServerComponents();
         
-        // builder.Services.AddFastEndpoints();
+        builder.Services.AddFastEndpoints(
+            options => options.Assemblies = moduleProvider.Assemblies);
+        
+        builder.Services.SwaggerDocument();
 
         // -------------------------------------------------------------------------------------------------------------
         // Application
@@ -36,11 +76,12 @@ public static class Program {
             app.UseHsts();
         }
 
+        app.UseFastEndpoints()
+            .UseSwaggerGen();
+        
         app.UseHttpsRedirection();
 
         app.UseAntiforgery();
-
-        // app.UseFastEndpoints();
 
         app.MapStaticAssets();
         app.MapRazorComponents<App>()
---
+++ src/application/InfiniLore.Application/InfiniLoreBuilder.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// Imports
+// ---------------------------------------------------------------------------------------------------------------------
+using InfiniLore.Core.Database;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InfiniLore.Application;
+// ---------------------------------------------------------------------------------------------------------------------
+// Code
+// ---------------------------------------------------------------------------------------------------------------------
+public static class InfiniLoreBuilder {
+    public static IServiceCollection AddInfiniLoreApplication(this IServiceCollection services) {
+        
+        return services;
+    }
+
+    public static WebApplication UseInfiniLoreApplication(this WebApplication app) {
+        
+        EnsureDatabaseCreated(app);
+
+        return app;
+    }
+    
+    private static void EnsureDatabaseCreated(WebApplication app) {
+        using IServiceScope scope = app.Services.CreateScope();
+        using var db = scope.ServiceProvider.GetRequiredService<InfiniLoreDb>();
+        db.Database.EnsureCreated();
+    }
+}
